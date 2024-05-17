33582422ee018d4fb22b595ca6c71dcae710398f
Raymond <rjrosales2025@gmail.com>
2024-05-17 02:56:48 -0400

    perf: Add async calls to argscore.py
    Admittedly, the boost in speed with this is negligible

--- argscore.py
+++ argscore.py
@@ -6,6 +6,13 @@ import get_advice as adv
 def clean_up_text(txt):
     return txt.lower()
 
+# The next two definitions are just asynchronous wrapper functions
+async def get_score(txt):
+    return pred.get_predicted_arg_score(txt)
+
+async def get_adv(txt, scr):
+    return adv.get_advice_f(txt, scr)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,11 +20,13 @@ def my_form():
     return render_template('form.html', input = "", score = "", advice = "")
 
 @app.route('/', methods=['POST'])
-def my_form_post():
+async def my_form_post():
     raw_text = request.form['text']
-    text = clean_up_text(raw_text)
-    score_float = pred.get_predicted_arg_score(text)
+    text     = clean_up_text(raw_text)
+    score_float = await get_score(text)
+    advice      = await get_adv(text, score_float)
     return render_template('form.html', 
-                           input = "Your argument, \"" + raw_text + "\", received a score of",
+                           input = "Your argument, \"" + raw_text +
+                                   "\", received a score of",
                            score = str(score_float),
-                           advice = adv.get_advice_f(text, score_float))
+                           advice = advice)
