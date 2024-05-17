f6928c5b5f0629e73f912e2712415e7b0a4aeb18
Raymond <rjrosales2025@gmail.com>
2024-05-16 23:40:28 -0400

    style: Add CSS to ArgScore web app
    Basic visual touch-ups to the web app, using simple CSS

--- argscore.py
+++ argscore.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    return render_template('form.html', score = "")
+    return render_template('form.html', input = "", score = "")
 
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
     answer = pred.get_predicted_arg_score(text.lower())
     # processed_text = str(answer)
-    return render_template('form.html', score = str(answer))
+    return render_template('form.html', input = "Your argument, \"" + text + "\", received a score of", score = str(answer))
